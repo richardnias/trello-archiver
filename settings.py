b132e8114fa699581473b1f850cc9f53e1f2d901
Richard Nias <richardnias@gmail.com>
2019-05-29 08:04:19 +0200

    initial commit

---
+++ settings.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TRELLO_API_KEY = os.environ['TRELLO_API_KEY']
+TRELLO_TOKEN = os.environ['TRELLO_TOKEN']
+TRELLO_DONE_LIST = os.environ['TRELLO_DONE_LIST']
