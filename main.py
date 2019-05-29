6e9a15b6c5a0bac5b1041fe5b16443475e8bf9c7
Richard Nias <richard.nias@native-instruments.de>
2019-05-29 11:15:25 +0200

    more functionality

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import requests
 import settings
-from typing import Dict
+from typing import Dict, List
 from urllib.parse import urlencode
 
 
-URL = "https://api.trello.com"
+StrDict = Dict[str, str]
+ListStrDict = List[StrDict]
+
+TRELLO_URL = "https://api.trello.com"
 
 
-def build_url(path: str, key: str, token: str, qs: Dict[str, str] = None) -> str:
+def build_url(path: str, key: str, token: str, qs: StrDict = None) -> str:
     """
     >>> build_url('/1/members', 'abc', '123')
     'https://api.trello.com/1/members?key=abc&token=123'
@@ -15,6 +18,22 @@ def build_url(path: str, key: str, token: str, qs: Dict[str, str] = None) -> str
     'https://api.trello.com/1/members?fields=id&key=abc+def&token=123'
     """
     full_qs = {**(qs if qs else {}), "key": key, "token": token}
-    return f"{URL}{path}?{urlencode(full_qs)}"
+    return f"{TRELLO_URL}{path}?{urlencode(full_qs)}"
 
 
+def get_cards(list_id: str, key: str, token: str) -> ListStrDict:
+    url = build_url(f'/1/lists/{list_id}/cards', key, token, qs={'fields': 'id,name'})
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+def construct_message(cards: ListStrDict) -> str:
+    message_header = 'Your weekly release 🦑:'
+    lines = [f'- {card["title"]}' for card in cards]
+    message = [message_header, ''] + lines
+    return '\n'.join(message)
+
+
+def send_sms(number: str, message: str):
+    pass
